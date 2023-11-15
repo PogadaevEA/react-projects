c8befc024ba2122cf7efd6cc420ad617cbe9c13c
Egor <egor.pogadaev@gmail.com>
2023-11-15 13:57:33 -0800

    Implement CSS Modules in React

---
+++ 23-css-modules/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Info from './components/Info'
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+      <h1>APP</h1>
+      <button className="my-button">APP button</button>
+    </div>
+  )
+}
+
+export default App
---
+++ 23-css-modules/src/components/Info.js
@@ -0,0 +1,13 @@
+import './Info.css'
+import styles from './Info.module.css'
+
+function Info() {
+  return (
+    <div className={styles.info}>
+      <h1>Info</h1>
+      <button className={styles.myAnotherButton}>click info</button>
+    </div>
+  )
+}
+
+export default Info
