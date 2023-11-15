052a4af919528223a5362f4e13b99861fbeb9df6
Egor <egor.pogadaev@gmail.com>
2023-11-15 05:44:47 -0800

    Use logical operators in JSX

---
+++ 15-logical-and-operator/src/components/ResetButton.js
@@ -0,0 +1,20 @@
+function ResetButton({ count, onClick }) {
+  const buttonStyle = { backgroundColor: 'lightgreen' }
+
+  return (
+    <div>
+      {count > 0 && (
+        <div>
+          <button
+            style={buttonStyle}
+            onClick={onClick}
+          >
+            Reset
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ResetButton
