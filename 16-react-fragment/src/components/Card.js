c1bac53507b364f2a957bcc74b4ec2128f3958fc
Egor <egor.pogadaev@gmail.com>
2023-11-15 06:03:40 -0800

    usage of React fragment

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+
+const Card = () => {
+    return (
+        <>
+            <h1>Egor Pogadaev</h1>
+            <h3>Fullstack Developer</h3>
+            <button>Like</button>
+        </>
+    )
+}
+
+export default Card
