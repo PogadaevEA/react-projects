6470670a2d78b109d32e1e9219b07202e69d5097
Egor <egor.pogadaev@gmail.com>
2023-11-16 12:23:24 -0800

    Simple reducer function with switch statement without redux

---
+++ 28-reducer/reducer.js
@@ -0,0 +1,29 @@
+const initialState = []
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'ADD_NAME':
+            return [...state, action.payload]
+        case 'DELETE_NAME':
+            return state.filter((name) => name !== action.payload)
+        case 'CLEAR_NAMES':
+            return []
+        default:
+            return state
+    }
+}
+
+let newState = reducer(initialState, { type: 'ADD_NAME', payload: 'John' })
+console.log(newState)
+
+newState = reducer(newState, { type: 'ADD_NAME', payload: 'Egor' })
+console.log(newState)
+
+newState = reducer(newState, { type: 'DELETE_NAME', payload: 'John' })
+console.log(newState)
+
+newState = reducer(newState, { type: 'CLEAR_NAMES' })
+console.log(newState)
+
+newState = reducer(newState, { type: 'ADD_NAME', payload: 'Egor' })
+console.log(newState)
