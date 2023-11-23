5041a03f70d4e0288356208ef7d912ffe969c1f1
Egor Pogadaev <egor.pogadaev@gmail.com>
2023-11-23 10:17:42 +0100

    Another option for extra reducers

--- 30-react-redux/frontend/src/redux/slices/bookSlice.js
+++ 30-react-redux/frontend/src/redux/slices/bookSlice.js
@@ -33,12 +33,12 @@ const booksSlice = createSlice({
       });
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(fetchBook.fulfilled, (state, action) => {
+  extraReducers: {
+    [fetchBook.fulfilled]: (state, action) => {
       if (action.payload.title && action.payload.author) {
         state.push(createBookWithId(action.payload, 'api'));
       }
-    });
+    },
   },
 });
 
